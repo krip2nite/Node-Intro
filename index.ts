143a7fb61bf2275595d19220bddd06d3ebb05385
krip2nite <kochnevandrey94@gmail.com>
2025-07-13 20:43:49 +0300

    reading file

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-import _ from 'lodash';
-
-const {argv} = process;
-const length = argv[2] ? +argv[2] : 5;
-const a: number[] = Array.from({length}, () => _.random(10, 20));
-console.log("random number are", a);
+import { readFile } from "node:fs/promises";
+const content =  await readFile(process.argv[1], {encoding: "utf-8"})
+console.log(content);
