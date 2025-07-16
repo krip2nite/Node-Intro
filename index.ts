8547eb74ec2bb9392017aaadcd956676b393a2ca
krip2nite <kochnevandrey94@gmail.com>
2025-07-16 20:59:01 +0300

    logger in emitter

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
-import EventEmitter from "events";
-const emitter = new EventEmitter();
-emitter.on("message", (arg: string) =>{
-    if (arg.toLocaleLowerCase().includes("hello")){
-        throw new Error("get tired from 'hello'")
-    }
-})
-emitter.on("message", (arg: any) => console.log("event message emitted",` message contains ${arg}`));
-emitter.on("message", (arg: string) => console.log(`length of message is ${arg.length}`));
-console.log(emitter.eventNames());
-emitter.emit("message", "kukureku");
-emitter.emit("message", "Hell world")
+import Logger from "./Logger.ts";
+import fs from "node:fs"
+const logger = new Logger();
+logger.addHandlerMessage((obj) => console.log(obj.message));
+logger.addHandlerLevel("debug", (message) => fs.writeFileSync("logs.txt", message + '\n', {flag: "a"}))
+logger.log("debug", "kukureku");
