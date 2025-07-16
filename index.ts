468c08e915e836334c8013d6b74bc4b15b557f1f
krip2nite <kochnevandrey94@gmail.com>
2025-07-16 19:51:42 +0300

    Base emitter functionality

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-import _ from 'lodash';
-
-const {argv} = process;
-const length = argv[2] ? +argv[2] : 5;
-const a: number[] = Array.from({length}, () => _.random(10, 20));
-console.log("random number are", a);
+import EventEmitter from "events";
+const emitter = new EventEmitter();
+emitter.on("message", (arg: string) =>{
+    if (arg.toLocaleLowerCase().includes("hello")){
+        throw new Error("get tired from 'hello'")
+    }
+})
+emitter.on("message", (arg: any) => console.log("event message emitted",` message contains ${arg}`));
+emitter.on("message", (arg: string) => console.log(`length of message is ${arg.length}`));
+console.log(emitter.eventNames());
+emitter.emit("message", "kukureku");
+emitter.emit("message", "Hell world")
