fa38036556122912cf545eb829a32c0e02f94554
krip2nite <kochnevandrey94@gmail.com>
2025-07-20 20:49:39 +0300

    using pipe with stdout (stream output)

--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,6 @@ creates large file of 0's 500mb sized
 
 time npm run dev
 in git bash, gives an option to see what time is needed
+
+time npm run dev > file.txt
+redirection into a file
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import CounterStream from "./CounterStream.ts";
 const counterStream = new CounterStream(100);
-counterStream.on('data', num => console.log(num));
-counterStream.on('end', () => console.log("all numbers stream"))
-counterStream.on('error', err =>console.log(err.message))
+counterStream.pipe(process.stdout)
+counterStream.on('end', () => process.stdout.write("\n")) // instead of stdout.flush
