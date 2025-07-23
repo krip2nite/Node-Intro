484696ad44f98f296c9fa5d1ca6a69f4271ad75b
krip2nite <kochnevandrey94@gmail.com>
2025-07-23 19:55:48 +0300

    writing a large file

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { createWriteStream } from "node:fs";
 const writeStream = createWriteStream("large_file",{highWaterMark: 1024 * 1024 * 10});
-const max = 1000000000;
-for (let i = 0; i < max; i++)
-{
-    writeStream.write("Hello");
-    i += 5;
+const max = 1_000_000;
+let index = 0;
+function write(){
+    let canWrite = true;
+    while(canWrite && index < max){
+        canWrite = writeStream.write("Hello".repeat(1000));
+        index += 5;
+    }
+    if (index < max){
+        writeStream.once("drain", write)
+    }
 }
-writeStream.end(()=> console.log("all data written"));
+write();
