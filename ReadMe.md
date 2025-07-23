4c59eabd59966e734105485cc5ed7e230115a6f0
krip2nite <kochnevandrey94@gmail.com>
2025-07-23 19:11:56 +0300

    writing in big files challenge

--- ReadMe.md
+++
@@ -1,8 +0,0 @@
-dd if=/dev/zero of=large_file bs=1M count=500
-creates large file of 0's 500mb sized
-
-time npm run dev
-in git bash, gives an option to see what time is needed
-
-time npm run dev > file.txt
-redirection into a file
--- index.ts
+++ index.ts
@@ -1,40 +1,9 @@
-import RandomNumbersStream from "./RandomNumbersStream.ts";
-import config from 'config'
-interface Params {
-    amount: number;
-    minValue: number;
-    maxValue: number;
+import { createWriteStream } from "node:fs";
+const writeStream = createWriteStream("large_file",{highWaterMark: 1024 * 1024 * 10});
+const max = 1000000000;
+for (let i = 0; i < max; i++)
+{
+    writeStream.write("Hello");
+    i += 5;
 }
-const AMOUNT_CONFIG_NAME = "amount";
-const MIN_VALUE_CONFIG_NAME = "min";
-const MAX_VALUE_CONFIG_NAME = "max";
-const DEFAULT_AMOUNT_VALUE = 7;
-const DEFAULT_MIN_VALUE = 1;
-const DEFAULT_MAX_VALUE = 49;
-function getParams(): Params {
-    const amount: number = getParam(AMOUNT_CONFIG_NAME, DEFAULT_AMOUNT_VALUE);
-    const minValue: number = getParam(MIN_VALUE_CONFIG_NAME, DEFAULT_MIN_VALUE);
-    const maxValue: number = getParam(MAX_VALUE_CONFIG_NAME, DEFAULT_MAX_VALUE);
-    return {amount, minValue, maxValue};
-
-}
-function getParam(configName: string, defaultValue: number): number {
-    return config.has(configName) ? config.get<number>(configName) : defaultValue;
-}
-async function displayUniqueRandomNumbers({amount, minValue, maxValue}): Promise<void> {
-
-    return new Promise((resolve, reject) => {
-        try {
-            const stream = new RandomNumbersStream(amount, minValue, maxValue, true);
-            stream.pipe(process.stdout);
-            stream.on("end", () => {
-                    process.stdout.write("\n");
-                    resolve();
-            })
-            stream.on("error", (err) => reject(err))
-        } catch(err) {
-            reject(err)
-        }
-    })
-}
-displayUniqueRandomNumbers(getParams()).catch(err => console.log(err.message))
+writeStream.end(()=> console.log("all data written"));
