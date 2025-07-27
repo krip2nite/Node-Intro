548f846bf943f152b7f7617346defd271ea0e07c
krip2nite <kochnevandrey94@gmail.com>
2025-07-27 18:26:42 +0300

    PipeLine example

--- index.ts
+++ index.ts
@@ -1,15 +1,59 @@
-import { createWriteStream } from "node:fs";
-const writeStream = createWriteStream("large_file",{highWaterMark: 1024 * 1024 * 10});
-const max = 1_000_000;
-let index = 0;
-function write(){
-    let canWrite = true;
-    while(canWrite && index < max){
-        canWrite = writeStream.write("Hello".repeat(1000));
-        index += 5;
+import { Readable, Writable, Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { TransformCallback } from "stream";
+class NumbersStream extends Readable{
+    private _counter = 0;
+    constructor(){
+        super({objectMode: true})
     }
-    if (index < max){
-        writeStream.once("drain", write)
+    _read(): void {
+        this.push(this._counter++)
     }
 }
-write();
+class EvenNumbers extends Transform{
+    constructor(){
+        super({objectMode: true})
+    }
+    _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void {
+        if(chunk  % 2 == 0){
+            this.push(chunk)
+        }
+        callback();
+    }
+}
+class Limit extends Transform{
+    private _counter = 0;
+    constructor(private _limit: number){
+        super({objectMode: true})
+    }
+    _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void {
+        if(this._counter >= this._limit){
+            this.push(null);
+        } else{
+            this.push(chunk);
+            this._counter++;
+            callback();
+        }
+    }
+}
+class OutputNumbers extends Writable{
+    constructor(){
+        super({objectMode: true})
+    }
+    _write(chunk: any, encoding: BufferEncoding, callback: (error?: Error | null) => void): void {
+        process.stdout.write(chunk + "; ");
+        callback();
+    }
+    _final(): void {
+        process.stdout.write("\n");
+    }
+}
+async function displayEvenNumbers(count: number): Promise<void>{
+    await pipeline(
+        new NumbersStream(),
+        new EvenNumbers(),
+        new Limit(count),
+        new OutputNumbers()
+    )
+}
+displayEvenNumbers(100).catch(err => console.log(err));
