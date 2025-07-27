cbffcc4c6134bc80a7b565a810cdb70f471fa841
krip2nite <kochnevandrey94@gmail.com>
2025-07-27 20:34:14 +0300

    initial calculator only normal flow

--- index.ts
+++ index.ts
@@ -1,53 +1,18 @@
-import displayUniqueRandomNumbers from "./display_unique_numbers.ts";
-import config from "config";
-interface Params {
-    minValue: number;
-    maxValue: number;
-    amount: number;
-    delimiter: string
-}
-const MIN_VALUE_CONFIG_NAME = "min";
-const MAX_VALUE_CONFIG_NAME = "max";
-const AMOUNT_VALUE_CONFIG_NAME = "amount";
-const DELIMITER_VALUE_CONFIG_NAME = "delimiter";
-const DEFAULT_DELIMITER_VALUE = "; ";
-const DEFAULT_MIN_VALUE = 1;
-const DEFAULT_MAX_VALUE = 49;
-const DEFAULT_AMOUNT = 7;
-function getParams(): Params {
-    const minValue = getParamNumber(MIN_VALUE_CONFIG_NAME, DEFAULT_MIN_VALUE);
-    const maxValue = getParamNumber(MAX_VALUE_CONFIG_NAME, DEFAULT_MAX_VALUE);
-    const amount = getParamNumber(AMOUNT_VALUE_CONFIG_NAME, DEFAULT_AMOUNT);
-    const delimiter = getParamString(DELIMITER_VALUE_CONFIG_NAME, DEFAULT_DELIMITER_VALUE);
-    return { minValue, maxValue, amount, delimiter };
-}
-function getParamNumber(configName: string, defaultValue: number): number {
-    const res: number = config.has(configName)
-        ? config.get<number>(configName)
-        : defaultValue;
-    if (typeof res != "number") {
-        throw new Error(
-            `Value of ${configName} parameter is "${res}" but must be a number`
-        );
+import { write } from 'node:fs';
+import http from 'node:http';
+const server = http.createServer();
+const port = 3500;
+server.listen(port, () => console.log("listening on port " + port));
+server.on("request", (req, res) => {
+    res.statusCode = 200;
+    const tockens = req.url.split("/")
+    let response: string = "";
+    switch(tockens[1]){
+        case "add": response = (+tockens[2] + +tockens[3]).toString(); break;
+        case "sub": response = (+tockens[2] - +tockens[3]).toString(); break;
+        case "div": response = (+tockens[2] / +tockens[3]).toString(); break;
+        case "mul": response = (+tockens[2] * +tockens[3]).toString(); break;
     }
-    return res;
-}
-function getParamString(configName: string, defaultValue: string): string {
-    const res: string = config.has(configName)
-        ? config.get<string>(configName)
-        : defaultValue;
-    if (typeof res != "string") {
-        throw new Error(
-            `Value of ${configName} parameter is "${res}" but must be a string`
-        );
-    }
-    return res;
-}
-
-(async () => {
-    try {
-        await displayUniqueRandomNumbers(getParams());
-    } catch (error) {
-        console.log(error.message);
-    }
-})();
+    res.write(response);
+    res.end();
+})
