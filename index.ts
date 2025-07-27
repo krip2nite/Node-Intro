321d27f25a2210acf770e1143e6392a9a61383da
krip2nite <kochnevandrey94@gmail.com>
2025-07-27 20:48:56 +0300

    processing data inside request using adding listeners

--- index.ts
+++ index.ts
@@ -1,18 +1,11 @@
-import { write } from 'node:fs';
 import http from 'node:http';
 const server = http.createServer();
 const port = 3500;
 server.listen(port, () => console.log("listening on port " + port));
 server.on("request", (req, res) => {
     res.statusCode = 200;
-    const tockens = req.url.split("/")
-    let response: string = "";
-    switch(tockens[1]){
-        case "add": response = (+tockens[2] + +tockens[3]).toString(); break;
-        case "sub": response = (+tockens[2] - +tockens[3]).toString(); break;
-        case "div": response = (+tockens[2] / +tockens[3]).toString(); break;
-        case "mul": response = (+tockens[2] * +tockens[3]).toString(); break;
-    }
-    res.write(response);
-    res.end();
+    let data = "";
+    req.on("data", chunk => data += chunk)
+    req.on("end", ()=> {res.write(data); res.end();})
+
 })
