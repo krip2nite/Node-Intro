54573539f42e848743992b5296fbf23417befadb
krip2nite <kochnevandrey94@gmail.com>
2025-09-13 16:46:26 +0300

    HW7 Post limit

--- src/controller/main.ts
+++ src/controller/main.ts
@@ -6,9 +6,9 @@ const port = 3500;
 app.listen(port, () => console.log("server is listening on port " + port));
 
 app.use(requestTime)
-app.post("/api/greet", (req: Request& {requestedAt: string}, res: Response) => {
+app.post("/api/greet", limiter, (req: Request& {requestedAt: string}, res: Response) => {
    res.json({message: "Hello!", requestedAt: req.requestedAt});
 })
-   app.get("/api/status", limiter, (req: Request& {requestedAt: string}, res: Response) => {
+   app.get("/api/status", (req: Request& {requestedAt: string}, res: Response) => {
    res.json({status: "Up and Running", requestedAt: req.requestedAt});
 } )
