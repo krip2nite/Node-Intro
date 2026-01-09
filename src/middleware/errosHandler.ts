3b4da5981f62af1db08fcdb6f4ce3214240dc136
krip2nite <kochnevandrey94@gmail.com>
2026-01-09 22:39:47 +0200

    ErrorHandler as separate module

--- src/middleware/errosHandler.ts
+++ src/middleware/errosHandler.ts
@@ -10,6 +10,7 @@ const errorHandler = (error: Error, _: Request, res: Response, __: NextFunction)
 }
 export default errorHandler;
 
+
 function getZodMessage(error: ZodError): string{
     return error.issues.reduce((res: string, iss) => res + `${res ? '; \n' : ''}${String(iss.path[0])}: ${iss.message}`, "")
 }
